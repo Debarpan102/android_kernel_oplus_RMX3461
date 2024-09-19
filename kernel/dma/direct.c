2c4f7273d2c9253bda891f5b42e4ba758fd797dd
Debarpan102 <91316829+Debarpan102@users.noreply.github.com>
2024-09-19 17:14:16 +0530

    direct.c : arch_sync_dma_for_device fixed

--- kernel/dma/direct.c
+++ kernel/dma/direct.c
@@ -360,7 +360,7 @@ dma_addr_t dma_direct_map_page(struct device *dev, struct page *page,
 	}
 
 	if (!is_dma_coherent(dev, attrs) && !(attrs & DMA_ATTR_SKIP_CPU_SYNC))
-		arch_sync_dma_for_device(phys_addr_t paddr, size_t size ,dir);
+		arch_sync_dma_for_device(paddr, size, dir);
 
 	return dma_addr;
 }
