573ed3516bc4e3dd15041c75377368087a75c39e
Debarpan102 <91316829+Debarpan102@users.noreply.github.com>
2024-09-23 11:32:42 +0530

    neighbour.c: fix more build errors

--- net/core/neighbour.c
+++ net/core/neighbour.c
@@ -249,10 +249,6 @@ static int neigh_forced_gc(struct neigh_table *tbl)
 			write_lock(&n->lock);
 			if ((n->nud_state == NUD_FAILED) ||
 			    (n->nud_state == NUD_NOARP) ||
-
-			  if  (tbl->is_multicast &&))
-			     tbl->is_multicast(n->primary_key)) ||
-			     time_after(tref, n->updated))
 			    !time_in_range(n->updated, tref, jiffies);
 
 				remove = true;
